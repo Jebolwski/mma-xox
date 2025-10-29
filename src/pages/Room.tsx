97d13d11eada4a1800598bb5ff17802c116c1796
jebolwski <mertgkmeen@gmail.com>
2025-10-29 20:30:07 +0300

    .

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -21,7 +21,7 @@ import return_img from "../assets/return.png";
 //import { Link } from "react-router-dom";
 import win from "../assets/sounds/win.mp3";
 import draw from "../assets/sounds/draw.mp3";
-import fighters_url from "../assets/data/fighters.json";
+import fighters_url from "../assets/data/fighters_updated_new.json";
 import wrong from "../assets/sounds/wrong.mp3";
 import correct from "../assets/sounds/correct.mp3";
 import Filters from "../logic/filters";
